555793d070611509ad26c1f99c7a4fa54d2610fc
Adam <a@gmail.com>
2026-04-08 12:30:07 +0200

    8042026

--- Zima2025C/Assets/Scripts/PauseMenuHandle.cs
+++ Zima2025C/Assets/Scripts/PauseMenuHandle.cs
@@ -1,8 +1,14 @@
+using JetBrains.Annotations;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PauseMenuHandle : MonoBehaviour
 {
     private GameStateMenager myGameStateMenagerRef;
+
+    public Slider Myslider;
+    public TMP_Text MyText;
     void Start()
     {
         var goGameMenagerRef = GameObject.FindGameObjectWithTag("GameController");
@@ -12,10 +18,23 @@ public class PauseMenuHandle : MonoBehaviour
     public void OnResumeButton_Clicked()
     {
         myGameStateMenagerRef.StartGame();
+
     }
+
+   
 
     public void OnExitButton_Clicked()
     {
         Application.Quit();
+    }
+
+    public void OnValue_Changed()
+    {
+        MyText.text = Myslider.value.ToString("F2"); 
+    }
+
+    public void OnValueWithParameter_Changed(float value)
+    {
+        MyText.text = value.ToString("F2");
     }
 }
